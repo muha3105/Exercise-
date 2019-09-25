7d203f02f00ad5953e19f76a5a8917f69830e7e5
Muddi <mudi_remmo@live.dk>
2019-09-25 13:53:09 +0200

    Initial commit

---
+++ src/hub.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class hub
+{
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+
+
+        double hourlyWage=9.25;
+
+        double workHours;
+
+        double pay;
+
+        double overtimeWage=hourlyWage*1.5;
+
+
+
+    }
+
+}
